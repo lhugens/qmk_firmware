fad1d73882827e98b547540426bcf44c7a2aac92
lhugens <leonardolobato@protonmail.com>
2020-07-13 11:26:40 +0100

    +*¨

--- keyboards/kyria/keymaps/lhugens/keymap.c
+++ keyboards/kyria/keymaps/lhugens/keymap.c
@@ -48,8 +48,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [QWERTY] = LAYOUT(
       KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC,
       KC_ESC,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, PT_TILD,
-      KC_LCTL, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_CCCV,   XXXXXXX, KC_DEL, KC_LEAD,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, PT_AT,
-              KC_PGUP, KC_LALT, KC_LGUI, MT(MOD_LSFT, KC_SPC), MO(RAISE), KC_ENT, KC_BSPC, MO(NAV), KC_RALT, KC_MUTE
+      KC_LCTL, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_CCCV,   XXXXXXX, KC_DEL, KC_LEAD,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, PT_PLUS,
+              KC_PGUP, KC_LALT, KC_LGUI, MT(MOD_LSFT, KC_SPC), MO(RAISE), KC_ENT, KC_BSPC, MO(NAV), KC_RALT, PT_AT 
     ),
 /*
  * Lower Layer: Numpad, Media
